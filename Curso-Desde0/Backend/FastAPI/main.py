6e54427758f2af926598cd2679d1af16baf05913
antobevi <abevilacqua@frba.utn.edu.ar>
2023-08-20 22:01:23 -0300

    Metodos HTTP + Mi primer API

---
+++ Curso-Desde0/Backend/FastAPI/main.py
@@ -0,0 +1,30 @@
+import fastapi
+
+app = fastapi.FastAPI()
+
+# Accedemos al contexto de FastAPI
+# El / es la raiz de donde se esta desplegando nuestra api, por ejemplo, http://127.0.0.1:8000/ es la raiz
+# Desde el navegador solo podemos hacer la operacion gET, si queremos hacer otras, vamos a necesitar una herramienta
+# como por ejemplo, Postman (cliente para ejecutar peticiones a un API)
+@app.get("/") # GET (obtener) es una de las operaciones disponibles dentro de la comunicacion HTTP
+async def get_string():
+    # Funcion asincrona
+    return "Siempre que llamamos a un servidor, las operaciones que se ejecutan deben ser Asincronas"
+
+# Petición Sincrona
+# Si llamamos al servidor, la aplicacion no puede hacer nada hasta que el servidor nos responda.
+# Espera a la respuesta del servidor para continuar.
+
+# FastAPI nos ofrece un servidor local para poder probar lo que estamos haciendo llamado Uvicorn.
+# Iniciar el servidor uvicorn: uvicorn main:app --reload
+# URL local: http://127.0.0.1:8000
+
+@app.get("/google-url")
+async def get_google_URL():
+    return {"google-url": "https://www.google.com.ar"}
+
+# Otras operaciones dentro de la comunicacion HTTP (solo las mas comunes, pero hay mas):
+# POST (crea un nuevo recurso)
+# PUT (actualiza un bloque completo. Por ejemplo, pasamos el usuario completo para que lo actualice)
+# PATCH (actualiza una parte de un bloque, no completo. Por ejemplo, del usuario solo pasamos el nuevo email)
+# DELETE
---
+++ Curso-Desde0/Backend/FastAPI/users.py
@@ -0,0 +1,107 @@
+import fastapi
+from pydantic import BaseModel
+
+# Creacion de una API simple para usuarios generales
+
+app = fastapi.FastAPI()
+
+# Para iniciar el servidor: uvicorn users:app --reload
+
+# Para crear una API de usuarios, primero necesitamos primero una entidad para definir usuarios
+"""
+class User:
+    name: str
+    surname: str
+    age: int
+    web_page_url: str
+
+    def __init__(self, name: str, surname: str, age: int, web_page_url: str) -> None:
+        self.__name = name # Propiedad privada
+        self.__surname = surname
+        self.__age = age
+        self.web_page_url = web_page_url # Propiedad publica
+
+    def get_name(self):
+        return self.name
+
+    def get_surname(self):
+        return self.surname
+
+    def get_age(self):
+        return self.age
+
+    def get_web_page_url(self):
+        return self.web_page_url
+"""
+# BaseModel nos brinca la capacidad de crear una entidad y la tratemos de diferentes formas, entonces, podemos crear la
+# entidad si hacer lo de antes y agregar el constructor y demas..
+class User(BaseModel): # User hereda un comportamiento de BaseModel
+    id: int
+    name: str
+    surname: str
+    age: int
+    web_page_url: str
+
+# Todavia no tenemos una bd con usuarios, asique creamos una lista de usuarios random
+users = [User(id = 1, name = "Antonella", surname = "Bevilacqua", age = 25, web_page_url = "https://www.linkedin.com/in/antonella-bevilacqua-a82631146/"),
+         User(id = 2, name = "Morita", surname = "Bevilacqua", age = 9, web_page_url = "No Page Found"),
+         User(id = 3, name = "Uma", surname = "Bevilacqua", age = 12, web_page_url = "No Page Found")]
+
+# GET
+@app.get("/users")
+async def get_users():
+    return users
+
+def search_user(id: int):
+    try:
+        return list(filter(lambda user: user.id == id, users))[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
+
+# Si nos piden un usuario en particular:
+# Path
+# Por ejemplo: /users/1
+@app.get("/users/{id}")
+async def get_specific_user(id: int):
+    return search_user(id)
+
+# Query
+# Por ejemplo: /users/?id=1&name="Antonella"
+@app.get("/users/")
+async def get_another_specific_user(id: int):
+    return search_user(id)
+
+# POST
+@app.post("/users/")
+async def new_user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe!"}
+    else:
+        users.append(user)
+        return {"result": "Usuario agregado con exito!"}
+
+# PUT
+# Actualizaremos un usuario completo, aunque solo queramo actualizar la url de su pagina web, por ejemplo.
+@app.put("/users/")
+async def update_user(user: User):
+    if type(search_user(user.id)) != User:
+        return {"error": "El usuario no existe!"}
+    else:
+        for index, saved_user in enumerate(users):
+            if saved_user.id == user.id:
+                users[index] = user  # Reemplazamos el objeto completo
+        return search_user(user.id)
+
+""" La función enumerate recibe un objeto iterable y retorna tuplas en las que cada una contiene 
+un elemento del objeto que recibe y un índice que indica su posición"""
+
+# DELETE
+@app.delete("/users/{id}")
+async def delete_user(id: int):
+    if type(search_user(id)) == User:
+        for index, saved_user in enumerate(users):
+            if saved_user.id == id:
+                users.pop(index)
+        return users
+    else:
+        return {"error": "El usuario no existe!"}
